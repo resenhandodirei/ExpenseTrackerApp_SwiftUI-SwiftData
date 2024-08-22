bd573a124df0a82c1bc35b172c3a5967b63adee7
Larissa Corrêa <larisol.corretora@gmail.com>
2024-08-22 20:49:25 -0300

    first commit

---
+++ ExpenseTrackerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTrackerApp
+//
+//  Created by Larissa Martins Correa on 22/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Larissa Martins Correa on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
