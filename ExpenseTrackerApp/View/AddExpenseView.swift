f16d10d09657bd02092321ca8ac498c6a8ad40a0
Larissa Corrêa <larisol.corretora@gmail.com>
2024-08-23 20:48:24 -0300

    tá dando erro nesse código, revisar

--- ExpenseTrackerApp/View/AddExpenseView.swift
+++ ExpenseTrackerApp/View/AddExpenseView.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  AddExpenseView.swift
 //  ExpenseTrackerApp
 //
@@ -8,8 +8,71 @@
 import SwiftUI
 
 struct AddExpenseView: View {
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.dismiss) private var context
+    
+    @State private var title: String = ""
+    @State private var subTitle: String = ""
+    @State private var date: Date = .init()
+    @State private var amount: CGFloat = 0
+    @State private var category: Category?
+    @Query(animation: .snappy) private var allCategories: [Category]
+     
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section("Title") {
+                    TextField("Magic Keyboard", text: $title)
+                }
+                
+                Section("Description") {
+                    HStack(spacing: 4) {
+                        Text("$")
+                            .fontWeight(.semibold)
+                        TextField("0.0", value: $amount, format: .currency(code: "US")  )
+                    }
+                }
+                Section("Date") {
+                    DatePicker("", selection: $date, displayedComponents: [.date])
+                        .datePickerStyle(.graphical)
+                        .labelsHidden()
+                }
+                if !allCategories.isEmpty {
+                    HStack {
+                        Text("Category")
+                        Spacer()
+                        Picker("", selection: $category) {
+                            ForEach(allCategories) {
+                                Text($0.categoryName)
+                                    .tag($0)
+                            }
+                        }
+                        .pickerStyle(.menu)
+                        .labelsHidden()
+                    }
+                }
+            }
+            .navigationTitle("Add Expense")
+            .toolbar {
+                // Button de cancelar e de adicionar
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .tint(.red)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add", action: addExpense ) {
+                    }
+            
+                }
+            }
+        }
+    
+    }
+    func addExpense() {
+        
     }
 }
 
