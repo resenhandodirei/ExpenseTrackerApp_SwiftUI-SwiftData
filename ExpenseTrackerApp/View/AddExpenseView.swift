5cbda3753d219c9e9d468a0264c1969870cd205e
Larissa Corrêa <larisol.corretora@gmail.com>
2024-08-23 20:20:13 -0300

    primeira tela funcionando

---
+++ ExpenseTrackerApp/View/AddExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseView.swift
+//  ExpenseTrackerApp
+//
+//  Created by Larissa Martins Correa on 23/08/24.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
